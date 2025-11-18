83f7d5265c15e1d31e586e572baff2a29b1f6f6f
Shaimaa-8 <shaimaaabushqair@gmail.com>
2025-11-18 13:51:17 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",   // مطابق لاسم الريبو على GitHub
+  base: "/Portfolio/",   // مطابق لاسم الريبو على GitHub
 })
